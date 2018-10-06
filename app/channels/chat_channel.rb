b0982324785cae52cc0490ff2c3e946fd8b5254b
Govind Jeevan <govindjeevan7@gmail.com>
2018-10-06 08:16:36 +0530

    first commit

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,11 @@
+class ChatChannel < ApplicationCable::Channel
+
+  def subscribed
+    stream_from 'chat'
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast('chat', data)
+  end
+
+end
